72ee7af8a8ac10411c65d8dfd92f4813a992097a
Aubin <aubinjos@hotmail.fr>
2024-05-17 13:41:44 +0200

    Commit de fin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,4 +17,15 @@ const config: Config = {
   },
   plugins: [],
 };
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+ 
+module.exports = withMT({
+  content: ["./pages/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
+
 export default config;
